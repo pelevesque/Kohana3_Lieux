11718f8d501f059cdc53644bf1bb4bff5a29642c
Pierre-Emmanuel Levesque <pierre.e.levesque@gmail.com>
2013-07-25 01:36:53 +0800

    separated methods in Model_lieux

--- classes/Kohana/model/lieux.php
+++ classes/Kohana/model/lieux.php
@@ -10,19 +10,42 @@ class Kohana_Model_Lieux extends Model_Database {
 	 */
 	public static function get_countries($db_table = 'countries')
 	{
-		$countries = DB::select()->from($db_table)->execute()->as_array();
-
-		if (I18n::$lang != 'en-us')
-		{
-			foreach($countries as $k => $country)
-			{
-				$countries[$k]['name'] = __($country['name']);
-			}
-
-			usort($countries, create_function('$a,$b', '$compare = strnatcmp(utf8::transliterate_to_ascii(utf8::strtolower($a["name"])), utf8::transliterate_to_ascii(utf8::strtolower($b["name"]))); return ($compare > 0) ? 1 : -1;'));
-		}
+		$countries = Model_lieux::fetch_all($db_table);
+		$countries = Model_lieux::translate($countries);
 
 		return $countries;
+	}
+
+	/**
+	 * Fetches all the contents of a database table.
+	 *
+	 * @param    string   database table
+	 * @return   array    database table
+	 */
+	protected static function fetch_all($db_table)
+	{
+		return DB::select()->from($db_table)->execute()->as_array();
+	}
+
+	/**
+	 * Translates an array's name field.
+	 *
+	 * @param    array    array to translate
+	 * @return   array    translated array
+	 */
+	protected static function translate($arr)
+	{
+		if (I18n::$lang != 'en-us')
+		{
+			foreach ($arr as $k => $v)
+			{
+				$arr[$k]['name'] = __($v['name']);
+			}
+
+			usort($arr, create_function('$a,$b', '$compare = strnatcmp(utf8::transliterate_to_ascii(utf8::strtolower($a["name"])), utf8::transliterate_to_ascii(utf8::strtolower($b["name"]))); return ($compare > 0) ? 1 : -1;'));
+		}
+
+		return $arr;
 	}
 
 } // End Kohana_Model_Lieux
